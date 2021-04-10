f478ec2d64cda0b05a82272176151a4a4980168a
Bernard Adhitya <bernard.adhitya@gmail.com>
2021-04-10 14:49:36 +0700

    [Bernard] Add more form elements on request job form

--- src/Containers/BusinessJobDetailPage/BusinessJobDetailPage.js
+++ src/Containers/BusinessJobDetailPage/BusinessJobDetailPage.js
@@ -1,4 +1,4 @@
-import { Grid } from '@material-ui/core';
+import { Grid, TextField } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { formattedCurrency } from '../../Constants/format';
@@ -9,6 +9,7 @@ const BusinessJobDetailPage = () => {
   const { id } = useParams();
 
   const [job, setJob] = useState(null);
+  const [selectedDate, setSelectedDate] = useState(new Date());
 
   useEffect(() => {
     const fetchData = async () => {
@@ -21,7 +22,109 @@ const BusinessJobDetailPage = () => {
   const renderRequestForm = () => {
     return job ? (
       <div className='request-form-card'>
-        <h3>{formattedCurrency(job.fee)}</h3>
+        <Grid container>
+          <Grid item xs={12} className='form-item'>
+            <h3>{`${formattedCurrency(job.fee)} per jam`}</h3>
+          </Grid>
+          <Grid item xs={12} className='form-item'>
+            <TextField
+              id="date"
+              label="Tanggal"
+              type="date"
+              defaultValue={selectedDate.toString()}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              variant="outlined"
+              fullWidth="true"
+            />
+          </Grid>  
+          <Grid item xs={12}>
+            <p>Jam</p>
+          </Grid>
+          <Grid item xs={12} className='form-item'>
+            <TextField
+              id="time"
+              label="Dari"
+              type="time"
+              defaultValue="07:30"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              inputProps={{
+                step: 300, // 5 min
+              }}
+              variant="outlined"
+              fullWidth="true"
+            />
+          </Grid>
+          <Grid item xs={12} className='form-item'>
+            <TextField
+              id="time"
+              label="Hingga"
+              type="time"
+              defaultValue="07:30"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              inputProps={{
+                step: 300, // 5 min
+              }}
+              variant="outlined"
+              fullWidth="true"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <p>Alamat</p>
+          </Grid>
+          <Grid item xs={6} className='form-item'>
+            <TextField
+              id="time"
+              label="Kota"
+              type="text"
+              defaultValue=""
+              variant="outlined"
+              fullWidth="true"
+            />
+          </Grid>
+          <Grid item xs={6} className='form-item'>
+            <TextField
+              id="time"
+              label="Kode Pos"
+              type="text"
+              defaultValue=""
+              variant="outlined"
+              fullWidth="true"
+            />
+          </Grid>
+          <Grid item xs={12} className='form-item'>
+            <TextField
+              id="time"
+              label="Alamat"
+              type="text"
+              defaultValue=""
+              variant="outlined"
+              fullWidth="true"
+              multiline
+              rows={4}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <p>Note</p>
+          </Grid>
+          <Grid item xs={12} className='form-item'>
+            <TextField
+              id="time"
+              label="Kota"
+              type="text"
+              defaultValue=""
+              variant="outlined"
+              fullWidth="true"
+              multiline
+              rows={4}
+            />
+          </Grid>
+        </Grid>
       </div>
     ) : <></>
   }
