182103f197a950189fad44af19d76c42f346e237
Bernard Adhitya <bernard.adhitya@gmail.com>
2021-04-17 10:20:03 +0700

    [Bernard] Add object literal for translated status

--- src/Constants/status.js
+++ src/Constants/status.js
@@ -7,3 +7,13 @@ export const STATUS = [
   'DONE',
   'REJECTED'
 ]
+
+export const TRANSLATED_STATUS = {
+  'ALL': 'Semua',
+  'WAITING-CONFIRMATION': 'Menunggu Konfirmasi',
+  'WAITING-PAYMENT': 'Menunggu Pembayaran',
+  'WAITING-PROGRESS': 'Menunggu Pengerjaan',
+  'IN-PROGRESS': 'Dalam Pengerjaan',
+  'DONE': 'Pekerjaan Selesai',
+  'REJECTED': 'Permintaan Ditolak'
+}
