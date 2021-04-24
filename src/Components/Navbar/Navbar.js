bd22f26cb9fec001baf17e21bec36df0a0849d0d
Bernard Adhitya <bernard.adhitya@gmail.com>
2021-04-24 12:03:58 +0700

    [Bernard] Switch role by navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,11 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { withStyles } from '@material-ui/core/styles';
 import './Navbar.css';
 import logo from '../../Assets/images/logo.png'
 import { Grid } from '@material-ui/core';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
+import {
+  Menu,
+  MenuItem,
+  ListItemIcon,
+  ListItemText
+} from '@material-ui/core';
+
+const StyledMenu = withStyles({
+  paper: {
+    border: '1px solid #d3d4d5',
+  },
+})((props) => (
+  <Menu
+    elevation={0}
+    getContentAnchorEl={null}
+    anchorOrigin={{
+      vertical: 'bottom',
+      horizontal: 'center',
+    }}
+    transformOrigin={{
+      vertical: 'top',
+      horizontal: 'center',
+    }}
+    {...props}
+  />
+));
+
+const StyledMenuItem = withStyles((theme) => ({
+  root: {
+    '&:focus': {
+      backgroundColor: theme.palette.primary.main,
+      '& .MuiListItemIcon-root, & .MuiListItemText-primary': {
+        color: theme.palette.common.white,
+      },
+    },
+  },
+}))(MenuItem);
 
 const Navbar = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleProfileClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   const history = useHistory();
+  const location = useLocation();
+
+  const currentRole = location.pathname.split('/')[1];
   
   const handleClick = (nav) => {
     const routes = {
@@ -17,6 +68,60 @@ const Navbar = () => {
     history.push(routes[nav]);
   }
 
+  const handleRedirectProfile = (option) => {
+    const routes = {
+      switch: currentRole === 'business' ? '/service' : '/business'
+    }
+    history.push(routes[option]);
+  }
+
+  const renderNavbarMenu = () => {
+    
+    return (
+      <div className='navbar-menu'>
+        <div className='item' onClick={() => handleClick('home')}>
+          <h5>{currentRole === 'business' ? 'Pekerjaan' : 'Status Pekerjaan'}</h5>
+        </div>
+        <div className='item' onClick={() => handleClick('orders')}>
+          <h5>Pesanan Pekerjaan</h5>
+        </div>
+        <div className='item' onClick={() => handleClick('chat')}>
+          <h5>Chat</h5>
+        </div>
+        <div>
+          <div
+            className='item'
+            onClick={handleProfileClick}
+          >
+            <h5>Profil</h5>
+          </div>
+          <StyledMenu
+            id="customized-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <StyledMenuItem onClick={() => handleRedirectProfile('switch')}>
+              <ListItemText
+                primary={currentRole === 'business' ? "Jadi Pekerja" : 'Cari Pekerja'}
+              />
+              <ListItemIcon/>
+            </StyledMenuItem>
+            <StyledMenuItem onClick={() => handleRedirectProfile('setting')}>
+              <ListItemText primary="Pengaturan" />
+              <ListItemIcon/>
+            </StyledMenuItem>
+            <StyledMenuItem onClick={() => handleRedirectProfile('logout')}>
+              <ListItemText primary="Keluar" />
+              <ListItemIcon/>
+            </StyledMenuItem>
+          </StyledMenu>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div class="navbar-wrapper">
       <Grid container spacing={3}>
@@ -25,20 +130,7 @@ const Navbar = () => {
         </Grid>
         <Grid item lg={7} md={6} sm={5} xs={4}></Grid>
         <Grid item lg={4} md={5} sm={6} xs={7}>
-          <div className='navbar-menu'>
-            <div className='item' onClick={() => handleClick('home')}>
-              <h5>Pekerjaan</h5>
-            </div>
-            <div className='item' onClick={() => handleClick('orders')}>
-              <h5>Pesanan Pekerjaan</h5>
-            </div>
-            <div className='item' onClick={() => handleClick('chat')}>
-              <h5>Chat</h5>
-            </div>
-            <div className='item' onClick={() => handleClick('profile')}>
-              <h5>Profil</h5>
-            </div>
-          </div>
+          {renderNavbarMenu()}
         </Grid>
       </Grid>
     </div>
