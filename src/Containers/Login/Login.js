51767e794ee397ea783dfba9408504558f147c19
Bernard Adhitya <bernard.adhitya@gmail.com>
2021-05-08 12:04:29 +0700

    [Bernard] Create switch for login and register form

--- src/Containers/Login/Login.js
+++ src/Containers/Login/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './Login.css';
-import { TextField } from '@material-ui/core';
+import { Grid, TextField } from '@material-ui/core';
 import { signIn } from '../../firebase';
 import { useHistory } from "react-router-dom"
 import { fetchCurrentUser } from '../../firebase';
@@ -8,15 +8,16 @@ import { fetchCurrentUser } from '../../firebase';
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [mode, setMode] = useState('login');
 
   const history = useHistory();
 
   const handleLogin = async () => {
-    console.log('email:', email);
-    console.log('password:', password);
     await signIn(email, password);
     const user = await fetchCurrentUser();
-    console.log(user);
+
     if (user) {
       history.push('/business');
     } else {
@@ -28,6 +29,33 @@ const Login = () => {
     <div className='login-page'>
       <div className='login-card'>
         <h3>Masuk ke PasarKerja</h3>
+        {
+          mode === 'register' &&
+          <div className="form-item">
+            <Grid container spacing={1}>
+              <Grid item xs={6}>
+                <TextField
+                  id="first-name"
+                  label="Nama Depan"
+                  variant="outlined"
+                  fullWidth="true"
+                  value={firstName}
+                  onChange={(e) => {setFirstName(e.target.value)}}
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <TextField
+                  id="last-name"
+                  label="Nama Belakang"
+                  variant="outlined"
+                  fullWidth="true"
+                  value={lastName}
+                  onChange={(e) => {setLastName(e.target.value)}}
+                />
+              </Grid>
+            </Grid>
+          </div>
+        }
         <div className="form-item">
           <TextField
             id="email"
@@ -51,9 +79,28 @@ const Login = () => {
         </div>
         <div className="form-item" onClick={() => handleLogin()}>
           <div className="login-btn">
-            Masuk
+            { mode === 'login' ? 'Masuk' : 'Daftar'}
           </div>
         </div>
+        { mode === 'login' ?
+          <p>
+            Belum punya akun? <span
+              onClick={() => setMode('register')}
+              style={{color: '#3183CD', cursor: 'pointer'}}
+            >
+              Daftar
+            </span>
+          </p>
+          :
+          <p>
+            Sudah punya akun? <span
+              onClick={() => setMode('login')}
+              style={{color: '#3183CD', cursor: 'pointer'}}
+            >
+              Login
+            </span>
+          </p>
+        }
       </div>
     </div>
   )
